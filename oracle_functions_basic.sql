1353ae23e3bdf3fcf21e4cff091f67fbfba930ab
인사팀 공용 <E7035@aes.hkmg.global>
2017-08-21 14:03:05 +0900

    날짜 표기

--- oracle_functions_basic.sql
+++ oracle_functions_basic.sql
@@ -11,3 +11,11 @@ FROM EMPLOYEES
 WHERE LOWER(FIRST_NAME) = 'alexis'; --칼럼자체를 조작함. 성능관점에서는 이렇게 하면 좋지 않다. 인덱스를 활용하지 못하게 된다. full scan이 일어나게 된다.
 
 select CONCAT('Good', 'String') FROM dual;
+
+SELECT SYSDATE FROM DUAL;
+
+--날짜 포맷
+SELECT TO_CHAR(TO_DATE('15-3-2016', 'DD-MM-YYYY'),' DD "OF" MONTH') FROM dual;
+
+--SYSDATE를 원하는 문자열로 바꾸려면 TO_CHAR로 바꾼다.
+SELECT TO_CHAR(SYSDATE, 'YYYY MM HH dy HH12:MI:SS') FROM dual;
