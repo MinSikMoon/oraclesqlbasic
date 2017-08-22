52d04168f30f17917912ca163850b4c919f8d2ba
인사팀 공용 <E7035@aes.hkmg.global>
2017-08-22 09:49:10 +0900

    outer join 실습

--- oracle_join.sql
+++ oracle_join.sql
@@ -11,8 +11,9 @@ WHERE EMPLOYEES.DEPARTMENT_ID = DEPARTMENTS.DEPARTMENT_ID;
 
 --pk는 이미 인덱스가 만들어져 있다. 
 SELECT E.FIRST_NAME, D.DEPARTMENT_NAME, L.CITY
-FROM EMPLOYEES E, DEPARTMENTS D, locations L
-WHERE (E.DEPARTMENT_ID = D.DEPARTMENT_ID) AND (D.location_id = l.location_id);
+FROM EMPLOYEES E, DEPARTMENTS D, LOCATIONS L
+WHERE (E.DEPARTMENT_ID = D.DEPARTMENT_ID) AND (D.location_id = l.location_id)
+ORDER BY L.city;
 
 --물리적으로 이렇게 붙은것에서 필요한 칼럼을 가져온 형태이다. 
 SELECT *
@@ -38,7 +39,27 @@ WHERE e.job_id = j.job_id
 AND   e.salary >= j.max_salary;
 
 --outer join : 정상적으로 조인 조건을 만족하지 못하는 행들을 보기위해서 쓴다. (+)을 쓴다. 조인시킬 값이 없는 조인측에 위치
+CREATE TABLE TEST_EMP
+AS SELECT * FROM EMPLOYEES;
 
+SELECT * FROM TEST_EMP;
+
+CREATE TABLE TEST_DEPT
+AS SELECT * FROM departments;
+
+SELECT * FROM test_dept;
+
+DELETE FROM TEST_DEPT WHERE department_id = 10;
+
+SELECT * FROM test_emp WHERE department_id = 10;
+SELECT * FROM test_DEPT WHERE department_id = 10; -- 현재 TEST_DEPT에서 ID가 10인 부서는 지워진 상태이다. 
+
+--당연히 결과값이 안나와야 하는 상황 -> 이럴때 OUTER JOIN을 쓰면 둘중 하나라도 있으면 나온다. (+)를 조건이 없는 쪽에 걸어준다!!
+SELECT * 
+FROM TEST_EMP E,
+     test_dept D
+WHERE e.department_id = d.department_id(+)
+AND e.department_id = 10;
 
 
 
