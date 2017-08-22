3e68dbdebdeec718b3eb046da78d967842ca07af
인사팀 공용 <E7035@aes.hkmg.global>
2017-08-22 16:56:07 +0900

    인덱스 실습

--- index.sql
+++ index.sql
@@ -1,2 +1,10 @@
 SELECT * FROM employees WHERE first_name = 'Alexis';
 
+--풀스캔을 탄다.
+SELECT * FROM departments WHERE department_name = 'Shipping';
+
+SELECT * FROM employees WHERE hire_date = '2007-06-21';
+
+--인덱스 달기
+CREATE INDEX ix_emp_01 on employees(HIRE_DATE);
+
